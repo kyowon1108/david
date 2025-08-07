09bd394c08f1813acb289208d7bf644ec9ac3077
이교원 <59288219+kyowon1108@users.noreply.github.com>
2025-08-07 14:37:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -26,5 +26,9 @@ def home():
 def menu():
     return render_template('menu.html')
 
+@app.route("/test2")
+def test2():
+    return render_template('test2.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', 80, debug=True)
